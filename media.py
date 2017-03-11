77f2222de3d00ed915c2c672caed6494e939dee7
Praveen Sankaranarayanan <pravs.91@gmail.com>
2017-03-11 11:38:39 -0800

    working trailer page
    A page to show movies, with embedded trailers and story line.

---
+++ media.py
@@ -0,0 +1,22 @@
+import webbrowser
+
+class Movie:
+	"""
+	A class to hold information about a movie.
+
+	Constructor args:
+	title (str): The movie title.
+	story_line (str): The movie's story line.
+	image_URL (str): A URL for the movie's poster.
+	trailer_URL (str): A URL for the movie's trailer.
+	"""
+	
+	def __init__(self,title,story_line,image_URL,trailer_URL):
+		self.title = title
+		self.story_line = story_line
+		self.poster_image_url = image_URL
+		self.trailer_youtube_url = trailer_URL
+
+	def playTrailer(self):
+		webbrowser.open(self.trailerURL)
+    
